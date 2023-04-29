cda6017d8d35f379ee27d728985d6944a078e0ea
virvira <v.saenko@skyeng.ru>
2023-04-29 18:53:55 +0300

    added vk authorization

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from core.views import UserCreateView, UserLoginView, ProfileView
+from core.views import UserCreateView, UserLoginView, ProfileView, UpdatePasswordView
 
 urlpatterns = [
     path('signup', UserCreateView.as_view(), name='signup'),
     path('login', UserLoginView.as_view(), name='login'),
     path('profile', ProfileView.as_view(), name='user'),
+    path('update_password', UpdatePasswordView.as_view(), name='update_password'),
 ]
