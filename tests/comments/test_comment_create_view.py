dd9ae6a06a139d65bebe1c996c7d867e28333cd9
virvira <v.saenko@skyeng.ru>
2023-05-10 19:07:01 +0300

    edited docker-compose

--- tests/comments/test_comment_create_view.py
+++ tests/comments/test_comment_create_view.py
@@ -1,11 +1,9 @@
-from typing import Any
 from unittest.mock import ANY
 
 import pytest
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APIClient
-from goals.models import GoalComment, Goal
+from goals.models import GoalComment
 
 
 @pytest.mark.django_db
@@ -19,20 +17,29 @@ class TestCommentCreateView:
         response = client.post(self.url, data={})
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    # def test_create_success(self, auth_client: APIClient, goal: Goal) -> None:
-    #     """
-    #     Успешное создание комментария
-    #     """
-    #     response = auth_client.post(self.url, data={
-    #         'text': 'Test comment',
-    #         'goal': goal.id
-    #     })
-    #     goal_comment = GoalComment.objects.last()
-    #     assert response.status_code == status.HTTP_201_CREATED
-    #     assert response.json() == {
-    #         'id': goal_comment.id,
-    #         'created': ANY,
-    #         'updated': ANY,
-    #         'text': 'Test comment',
-    #         'goal': goal_comment.goal.id
-    #     }
+    def test_create_without_goal(self, auth_client):
+        """
+        При создании комментария авторизованный пользователь не передает параметром цель и получает ошибку
+        """
+        response = auth_client.post(self.url, data={
+            'title': 'Comment',
+        })
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_create_success(self, auth_client, goal):
+        """
+        Авторизованный пользователь успешно создает комментарий
+        """
+        response = auth_client.post(self.url, data={
+            'text': 'Test comment',
+            'goal': goal.id
+        })
+        goal_comment = GoalComment.objects.last()
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.json() == {
+            'id': goal_comment.id,
+            'created': ANY,
+            'updated': ANY,
+            'text': 'Test comment',
+            'goal': goal_comment.goal.id
+        }
